a261e879e893dbcd65a6790d5f4c76ff03da3bdd
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-24 17:24:16 +0700

    router and store

---
+++ client/src/store/modules/movies.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+
+const state = {
+  movies: null,
+  movieDetail: null
+}
+
+const getters = {
+  getAllMovie(state) {
+    return state.movies;
+  },
+  getMovie(state) {
+    return state.movieDetail;
+  }
+}
+
+const mutations = {
+  "get all movie"(state, data) {
+    state.movies = data;
+  },
+  "get movie"(state, data) {
+    state.movieDetail = data;
+  }
+}
+
+const actions = {
+  "get all movie"({ commit }) {
+    axios({
+      url: "/movies",
+      method: "get"
+    })
+      .then(({ data }) => {
+        commit("get all movie", data);
+      }).catch((err) => {
+        console.error(err);
+      });
+  },
+  "get movie"({ commit }, movieId) {
+    axios({
+      url: `/movies/${movieId}`,
+      method: "get"
+    })
+      .then(({ data }) => {
+        commit("get movie", data);
+    }).catch((err) => {
+          console.error(err);
+        });
+  },
+  "delete rate"(context, payload) {
+    axios({
+      url: `/rates/${payload.rateId}`,
+      method: "delete"
+    })
+    .then(() => {
+      context.dispatch("get movie", payload.movieId)
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
