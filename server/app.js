dee200677d0e8938707116b673cdf496b937dfb1
Okka Linardi <okka.linardi@gmail.com>
2020-02-24 13:45:26 +0700

    Finished express skeleton

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const router = require('./routes')
+const port = process.env.PORT || 3000
+
+app
+    .use(cors())
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use('/', router)
+    .listen(port, () => {
+        console.log('server is running on port: ' + port)
+    })
