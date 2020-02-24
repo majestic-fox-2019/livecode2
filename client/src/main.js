098e1292f0efb89185c042299fba082f156b77b4
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-24 17:25:13 +0700

    Home page

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,17 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import axios from 'axios';
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+axios.defaults.baseURL = "http://localhost:3000"
+
 Vue.config.productionTip = false
+
+Vue.use(axios);
 
 new Vue({
   router,
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-jumbotron header="Welcom to Movie Reviewer"> </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col
+          class="mb-5"
+          cols="12"
+          v-for="movie in getAllMovie"
+          :key="movie.id"
+        >
+          <b-card no-body class="overflow-hidden">
+            <b-row no-gutters>
+              <b-col md="4">
+                <b-card-img
+                  :src="movie.poster"
+                  class="rounded-0 img-fluid"
+                ></b-card-img>
+              </b-col>
+              <b-col md="8">
+                <b-card-body class="text-center p-5">
+                  <b-card-text class="p-5">
+                    <h4 class="display-4">{{ movie.title }}</h4>
+                    <p class="lead">{{ movie.year }}</p>
+                    <div class="mb-4">
+                      <a
+                        :href="`https://www.imdb.com/title/${movie.imdbID}`"
+                        target="_blank"
+                      >
+                        <b-button variant="outline-primary">IMDB</b-button>
+                      </a>
+                    </div>
+                    <div>
+                      <b-button :to="`/${movie.id}`">
+                        Details
+                      </b-button>
+                    </div>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapGetters } from "vuex";
+// import axios from 'axios';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  beforeCreate() {
+    const vm = this;
+    vm.$store.dispatch("get all movie");
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getAllMovie"])
   }
-}
+};
 </script>
