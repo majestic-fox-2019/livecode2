289593e9576b56bb7290850292f28742c3ff8596
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-02-24 13:55:30 +0700

    databases

---
+++ server/models/rate.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Rate = sequelize.define('Rate', {
+    reviewer: DataTypes.STRING,
+    point: DataTypes.INTEGER,
+    MovieId: DataTypes.INTEGER
+  }, {});
+  Rate.associate = function(models) {
+    // associations can be defined here
+  };
+  return Rate;
+};
